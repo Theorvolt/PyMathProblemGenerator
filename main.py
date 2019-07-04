d0f66f76b2bfdee9a8a1a2cd0c4b673b2a0d2395
Sai kumar <sai494iscool@gmail.com>
2019-07-04 21:05:27 +1000

    File Uploads
    Added initial versions of the Problem Generator and the Problem designer.

---
+++ main.py
@@ -0,0 +1,162 @@
+from mainwindow import Ui_MainWindow  # importing our generated file
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib
+from wolframclient.language import wl
+from wolframclient.language import wlexpr
+from wolframclient.evaluation import WolframLanguageSession
+from PIL import Image
+from wolframclient.language import Global
+from PyQt5.QtGui import QIcon, QPixmap
+import sys
+import sip
+
+class mywindow(QtWidgets.QMainWindow):
+ 
+	def __init__(self):
+
+		super(mywindow, self).__init__()
+		print(session.evaluate('''Directory[]'''))
+		self.imageQuestion = False
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+
+		# https://stackoverflow.com/questions/41247109/pyqt-how-to-switch-widgets-in-qstackedwidget
+		# We can set up button functionality here instead.
+		self.ui.toMainMenu.clicked.connect(self.refreshMenu)
+		# then at either point regenerate the question by running the function
+
+		# self.whatever.clicked.connect(self.newQuestion)
+		self.ui.toMainMenu_2.clicked.connect(self.refreshMenu)
+		self.ui.toMainMenu_3.clicked.connect(self.refreshMenu)
+
+		self.ui.toSettings.clicked.connect(self.refreshSettings)
+
+		self.ui.toQuestion.clicked.connect(self.newQuestion)
+		self.ui.toQuestion_2.clicked.connect(self.deleteOldQuestion)
+
+		self.ui.toAnswer.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(3))
+		self.ui.difficultybox.addItems(["Easy","Normal","Hard"])
+		topics=session.evaluate('''Keys[questionRepo["Easy"]]''')
+		self.ui.topicbox.addItems(list(topics))
+
+		# to access difficulty do self.ui.difficultybox.currentText()
+
+	def refresh(self):
+		if self.ui.verticalLayout_5.count() < 3:
+			return
+		else:
+			if self.ui.verticalLayout.count() == 4:
+				self.ui.verticalLayout.removeWidget(self.label1)
+				self.label1.deleteLater()
+				self.label1 = None
+
+			self.ui.verticalLayout_5.removeWidget(self.canvasQ)
+			self.canvasQ.deleteLater()
+			self.canvasQ = None
+			self.ui.verticalLayout.removeWidget(self.canvasA)
+			self.canvasA.deleteLater()
+			self.canvasA = None
+
+	def refreshSettings(self):
+		self.ui.stackedWidget.setCurrentIndex(1)
+		self.refresh()
+
+	def refreshMenu(self):
+		self.ui.stackedWidget.setCurrentIndex(0)
+		self.refresh()
+
+
+	def newQuestion(self):
+		topic=self.ui.topicbox.currentText()
+		difficulty=self.ui.difficultybox.currentText()
+		self.ui.stackedWidget.setCurrentIndex(2)
+		qa=session.evaluate(wlexpr('''evalQuestion["'''+str(topic)+'''","'''+str(difficulty)+'''"]'''))
+		q =qa[0]
+		a=qa[1]
+		img = qa[2]
+
+		bg = self.palette().window().color()
+		cl = (bg.redF(), bg.greenF(), bg.blueF())
+		self.question = Figure(facecolor=cl,edgecolor=cl)
+		self.canvasQ = FigureCanvas(self.question)
+		self.ui.verticalLayout_5.addWidget(self.canvasQ)
+
+		self.question.suptitle(q,
+				x=0.0, y=0.5, 
+				horizontalalignment='left',
+				verticalalignment='bottom',
+				fontsize=10)
+		#self.question.text(x=0.0,y=0.5,s="HELLO $2^{3+4}$")
+		self.canvasQ.draw()
+
+		self.answer = Figure(facecolor=cl,edgecolor=cl)
+		self.canvasA = FigureCanvas(self.answer)
+
+		self.ui.verticalLayout.addWidget(self.canvasA)
+		self.answer.suptitle(a,
+				x=0.0, y=0.6, 
+				horizontalalignment='left',
+				verticalalignment='top',
+				fontsize=10)
+		#self.answer.text(x=0.0,y=0.5,s="HELLO $2^{3+4}$")
+		self.canvasA.draw() # dies here
+
+		if img != 0:
+			self.imageQuestion = True
+			self.label1 = QtWidgets.QLabel(self)
+			self.pixmap1 = QPixmap(img)
+			self.label1.setPixmap(self.pixmap1)
+			self.resize(self.pixmap1.width(), self.pixmap1.height())
+			self.ui.verticalLayout.addWidget(self.label1)
+			self.show()
+
+		else:
+			self.imageQuestion = False
+
+
+		
+		# add bit with getting information from mathematica then setting all the required fields
+
+	def deleteOldQuestion(self):
+		self.ui.verticalLayout_5.removeWidget(self.canvasQ)
+		self.canvasQ.deleteLater()
+		self.canvasQ = None
+
+		self.ui.verticalLayout.removeWidget(self.canvasA)
+		self.canvasA.deleteLater()
+		self.canvasA = None
+
+		if self.imageQuestion == True:
+
+			self.ui.verticalLayout.removeWidget(self.label1)
+			self.label1.deleteLater()
+			self.label1 = None
+
+
+		self.newQuestion()
+
+
+
+
+
+if __name__ == '__main__':
+	session = WolframLanguageSession()
+	commands = [
+	'''Get["definitions.mx"];''',
+	'''questionRepo=Quiet@Import["repo.mx"];'''
+	]
+	for i in commands:
+		session.evaluate(wlexpr(i))
+		
+	app = QtWidgets.QApplication([])
+	application = mywindow()
+	application.show()
+	try:
+		sys.exit(app.exec())
+	except SystemExit:
+		session.terminate()		
+		print("Wolfram Link has been terminated.")
+
---
+++ maindesign.py
@@ -0,0 +1,72 @@
+from designer import Ui_designer
+from PyQt5 import QtWidgets
+from wolframclient.language import wl
+from wolframclient.language import wlexpr
+from wolframclient.evaluation import WolframLanguageSession
+from PIL import Image
+from wolframclient.language import Global
+from PyQt5.QtGui import QIcon, QPixmap
+import sys
+
+class mywindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		super(mywindow, self).__init__()
+		self.ui = Ui_designer()
+		self.ui.setupUi(self)
+		self.ui.pushButton_2.clicked.connect(lambda :self.ui.stackedWidget.setCurrentIndex(1))
+		self.ui.pushButton_5.clicked.connect(lambda :self.ui.stackedWidget.setCurrentIndex(0))
+		topics=session.evaluate('''Keys[questionRepo["Easy"]]''')
+		self.ui.comboBox_4.addItems(list(topics))
+		self.ui.comboBox.addItems(['Easy','Normal','Hard'])
+		self.ui.comboBox_3.addItems(list(topics))
+		self.ui.pushButton_3.clicked.connect(self.addTopic)
+		self.ui.pushButton_4.clicked.connect(self.deleteTopic)
+		self.ui.pushButton.clicked.connect(self.compile)
+
+	def deleteTopic(self):
+		print(session.evaluate(wlexpr('''Keys[questionRepo["Easy"]]''')))
+		selectedTopic = self.ui.comboBox_3.currentText()
+		topicIndex = self.ui.comboBox_3.currentIndex()
+		self.ui.comboBox_3.removeItem(topicIndex)
+		self.ui.comboBox_4.removeItem(topicIndex)
+		session.evaluate(wlexpr('''deleteTopic[ToString['''+str(selectedTopic)+''']]'''))
+		session.evaluate(wlexpr('''Export["repo.mx",questionRepo]'''))
+		print(session.evaluate(wlexpr('''Keys[questionRepo["Easy"]]''')))
+
+
+	def addTopic(self):
+		print(session.evaluate(wlexpr('''Keys[questionRepo["Easy"]]''')))
+		topic = self.ui.lineEdit_5.text()
+		session.evaluate(wlexpr('''addNewTopic[ToString['''+str(topic)+''']]'''))
+		self.ui.comboBox_4.addItem(topic)
+		self.ui.comboBox_3.addItem(topic)
+		session.evaluate(wlexpr('''Export["repo.mx",questionRepo]'''))
+		print(session.evaluate(wlexpr('''Keys[questionRepo["Easy"]]''')))
+
+	def compile(self):
+		print('''Hold[Export["0.png",Rasterize[GraphicsRow[{'''+self.ui.lineEdit_6.text()+'''}]]]]''')
+		print('''ToString['''+self.ui.lineEdit_3.text()+''']''')
+		print('''Hold['''+self.ui.lineEdit_2.text()+''']''')
+		print(self.ui.lineEdit.text())
+		print('''Hold['''+self.ui.lineEdit_4.text()+'''''')
+
+
+
+
+if __name__ == '__main__':
+	session = WolframLanguageSession()
+	commands = [
+	'''Get["definitions.mx"];''',
+	'''questionRepo=Quiet@Import["repo.mx"];'''
+	]
+	for i in commands:
+		session.evaluate(wlexpr(i))
+		
+	app = QtWidgets.QApplication([])
+	application = mywindow()
+	application.show()
+	try:
+		sys.exit(app.exec())
+	except SystemExit:
+		session.terminate()		
+		print("Wolfram Link has been terminated.")
